e940f2df956950db91089b32c98858fe9bccb4d5
Aleu <111565097+Aleu0091@users.noreply.github.com>
2024-12-21 12:55:48 +0900

    Create gemini.js

---
+++ src/gemini.js
@@ -0,0 +1,34 @@
+const {
+    HarmBlockThreshold,
+    HarmCategory,
+    GoogleGenerativeAI,
+} = require("@google/generative-ai");
+const fs = require("fs");
+const path = require("path");
+
+const safetySettings = [
+    {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+];
+
+const genAI = new GoogleGenerativeAI(require("../../config.json").geminiAPIKey); // 니네 지미나이 키 경로
+const systemInstructionPath = path.join(__dirname, "system_instruction.txt");
+
+const fileContent = fs.readFileSync(systemInstructionPath, "utf8");
+
+const lines = fileContent.split("\n").filter((line) => line.trim() !== "");
+const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash-001",
+    systemInstruction: {
+        parts: lines.map((line) => ({ text: line })),
+    },
+    generation_config: {
+        temperature: 1.2,
+        max_output_tokens: 8192,
+        response_mime_type: "text/plain",
+    },
+    safetySettings,
+});
+module.exports = { model };
